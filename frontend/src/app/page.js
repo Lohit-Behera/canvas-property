7e30d21217c3f2dea987073cf8e88409bab73930
lohit <lohitsekhar.2002@gmail.com>
2025-01-14 18:30:21 +0530

    add property listing display and integrate Redux for state management

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,11 +1,32 @@
 "use client";
 
 import { withAuth } from "@/components/withAuth";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchGetAllProperties } from "@/lib/features/propertySlice";
+import { useEffect } from "react";
 
 function Home() {
+  const dispatch = useDispatch();
+
+  const properties = useSelector(
+    (state) => state.property.getAllProperties.data
+  )
+  useEffect(() => {
+    dispatch(fetchGetAllProperties());
+  }, []);
   return (
-   <div>
-    Home Page
+   <div className="p-4 rounded-md border w-full md:w-[95%] grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+    {properties.map((property) => (
+      <div key={property._id} className="p-2 rounded-md border">
+        <img
+          src={property.thumbnail}
+          alt={property.title}
+          className="w-full h-44 rounded-md object-cover"
+        />
+        <h3 className="text-base md:text-lg font-semibold ">{property.title}</h3>
+        <p className="text-sm text-muted-foreground line-clamp-3">{property.description}</p>
+      </div>
+    ))}
    </div>
   );
 }
--- frontend/src/lib/store.js
+++ frontend/src/lib/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userSlice from "./features/userSlice";
+import propertySlice from "./features/propertySlice";
 
 export const makeStore = () => {
   return configureStore({
     reducer: {
       user: userSlice,
+      property: propertySlice,
     },
   });
 };
